572c363c5d96d470202d0ddad87b578e7118af27
Ray_R <Ray_R@DESKTOP-9LRPCH2>
2021-01-14 08:39:22 -0600

    Added Product class which should have existed prior to me re-adding it.

---
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        [StringLength(20)]                   //requires System.ComponentModel.DataAnnotations 
+        [DisplayName("Product Name")]        //requires System.ComponentModel;
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Range(0, 1000)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        /* I like to add my own constructor for product as this will allow us to have flexibility in database technologies we can use*/
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString(); //Generated ID converted to a string will be stored to an Id.
+        }
+    }
+}
